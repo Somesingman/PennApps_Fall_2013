36c3ce754c2f28914b7dedb9aa7a9a0d39094fb8
Jon <jonchen42@gmail.com>
2013-09-07 21:13:01 -0400

    Added isDominant() function

---
+++ TestDominant.java
@@ -0,0 +1,27 @@
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+
+import org.junit.Test;
+
+
+public class TestDominant {
+
+	@Test
+	public void testDominant1() {
+		Converter converter = new Converter();
+		String key = "C";
+		ArrayList<Note> notes = new ArrayList<Note>();
+		Note g = new Note();
+		g.setStep("G");
+		Note b = new Note();
+		b.setStep("B");
+		Note d = new Note();
+		d.setStep("D");
+		notes.add(g);
+		notes.add(b);
+		notes.add(d);
+		assertTrue(converter.isDominant(notes,key));
+	}
+
+}
